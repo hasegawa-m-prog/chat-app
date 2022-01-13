652ed52731c0e4dd219ae309ad08624828100190
hasegawa-m-prog <hasegawa83838376@gmail.com>
2022-01-13 16:08:18 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2022_01_12_114503) do
-
   create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "room_id", null: false
     t.bigint "user_id", null: false
